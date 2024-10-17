f3985a45415c196d4bc3649e92f4803959ee83da
Caio Montilha <94456335+cmontilha@users.noreply.github.com>
2024-10-17 08:13:15 -0400

    Add transaction creation functionality

--- blockchain.py
+++ blockchain.py
@@ -7,15 +7,15 @@ class Blockchain:
         self.chain = []
         self.current_transactions = []
 
-        # Create the genesis block
+        # Cria o bloco gênesis
         self.new_block(previous_hash='1', proof=100)
 
     def new_block(self, proof, previous_hash=None):
         """
-        Create a new block in the blockchain
-        :param proof: <int> The proof given by the Proof of Work algorithm
-        :param previous_hash: (Optional) <str> Hash of the previous block
-        :return: <dict> New Block
+        Cria um novo bloco na blockchain
+        :param proof: <int> A prova dada pelo algoritmo de Proof of Work
+        :param previous_hash: (Opcional) <str> Hash do bloco anterior
+        :return: <dict> Novo Bloco
         """
         block = {
             'index': len(self.chain) + 1,
@@ -25,8 +25,39 @@ class Blockchain:
             'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
 
-        # Reset the list of current transactions
+        # Reseta a lista de transações atuais
         self.current_transactions = []
 
         self.chain.append(block)
         return block
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Cria uma nova transação para ser incluída no próximo bloco minerado
+        :param sender: <str> Endereço do remetente
+        :param recipient: <str> Endereço do destinatário
+        :param amount: <int> Quantidade
+        :return: <int> O índice do bloco que conterá a transação
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+        """
+        Cria um hash SHA-256 de um bloco
+        :param block: <dict> Bloco
+        :return: <str> Hash do bloco
+        """
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        # Retorna o último bloco da cadeia
+        return self.chain[-1]
