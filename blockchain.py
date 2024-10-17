58e661955e1eeee029eb2ddf303e0c3c0eb2ffa8
Caio Montilha <94456335+cmontilha@users.noreply.github.com>
2024-10-17 08:07:35 -0400

    Add initial Blockchain class structure and genesis block creation

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,32 @@
-# Initial Commit
+import hashlib
+import json
+from time import time
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+        # Create the genesis block
+        self.new_block(previous_hash='1', proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new block in the blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of the previous block
+        :return: <dict> New Block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the list of current transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
